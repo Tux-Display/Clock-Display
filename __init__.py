0f9fd630c318fe6f1cee3d205cad1e50d831dba4
Crunch <91413622+CrunchV1@users.noreply.github.com>
2022-04-08 21:53:32 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@
 from calendar import c
 from mycroft import MycroftSkill, intent_file_handler
 
+##pip install nog?
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
@@ -12,39 +13,37 @@ class ClockForSchoolDieWantIi(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
+    ##we moeten eig. bij elke lijn neer zetten wat het doen
+    ##later handig voor presentaties
 
-
-
-	# start automatisch homescreen op wanneer mycroft skill geladen is, althans, in theorie
-    #chrome browser settings
-    chrome_options = Options()
-
-    # Verwijderd infobars -> bar die liet weten dat het automated was
-    chrome_options.add_experimental_option("useAutomationExtension", False)
-    chrome_options.add_experimental_option("excludeSwitches",["enable-automation"])
-
-    # chrome kios mode -> geen functies zodat de gebruiker niks kan doen
-    chrome_options.add_argument("--kiosk")
-    chrome_options.add_argument("--disable-application-cache")
-    chrome_options.add_argument("disable-infobars")
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-
-    # open home pagina
-    driver.get("file:///opt/mycroft/skills/X-man-homescreen-css/index.html")
-
-
-	#Het bestand van commands die klok laten zien
+    #Het bestand van commands die klok laten zien
     @intent_file_handler('ii.want.die.school.for.clock.intent')
-    
+
     #de functie die mycroft runt wanneer de gebruiker erom gevraagd heeft
     def handle_ii_want_die_school_for_clock(self, message):
-        # print("function clock activated")
-        
+
         #Verteld de gebruiker na het activeren van clock command dat de klok op gaat starten
-    	self.speak_dialog('ii.want.die.school.for.clock')
+        self.speak_dialog('ii.want.die.school.for.clock')
 
+        ##Eig. moet pi altijd in desktop env. opstarten
+        ##en dan standaard klok
 
+        #chrome browser voor de klok
+        chrome_options = Options()
 
+        # Verwijderd infobars -> bar die liet weten dat het automated was
+        chrome_options.add_experimental_option("useAutomationExtension", False)
+        chrome_options.add_experimental_option("excludeSwitches",["enable-automation"])
+
+        # chrome kios mode -> geen functies zodat de gebruiker niks kan doen
+        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--kiosk")
+        chrome_options.add_argument("--disable-application-cache")
+        chrome_options.add_argument("disable-infobars")
+        driver = webdriver.Chrome(chrome_options=chrome_options)
+
+        # open home pagina
+        driver.get("file:///opt/mycroft/skills/X-man-homescreen-css/homescreen.html")
 
 #verteld aan mycroft de juiste function name voor deze skill
 def create_skill():
