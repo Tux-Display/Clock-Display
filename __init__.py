a62953b0c0672cbd7c82b82f427edd7aef3fc559
Crunch <91413622+CrunchV1@users.noreply.github.com>
2022-05-11 15:30:25 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,9 @@
 #Het includen/invoegen van benodigden libraries
 
 # from calendar import c
-# import os
+import os
 from mycroft import MycroftSkill, intent_file_handler
 
-
-# print(os.system("cat /proc/self/environ"))
 
 ## NOTES
 ## wat nou als we de driver variable blok stuk alles globaal maken, ergens in mycroft gedifned
@@ -16,12 +14,14 @@ from mycroft import MycroftSkill, intent_file_handler
 class ClockForSchoolDieWantIi(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        
 
     #Het bestand van commands die klok laten zien
     @intent_file_handler('ii.want.die.school.for.clock.intent')
 
     #de functie die mycroft runt wanneer de gebruiker erom gevraagd heeft
     def handle_ii_want_die_school_for_clock(self, message):
+        print(os.system("cat /proc/self/environ >> /tmp/test.txt"))
 
         #Verteld de gebruiker na het activeren van clock command dat de klok op gaat starten
         self.speak_dialog('ii.want.die.school.for.clock')
