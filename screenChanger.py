58136911146e9953c04b82e6c88ee869dcbad7af
Crunch <91413622+CrunchV1@users.noreply.github.com>
2022-05-13 15:35:55 +0200

    Update screenChanger.py

--- screenChanger.py
+++ screenChanger.py
@@ -20,7 +20,7 @@ while True:
     oude = ""
     with open('/tmp/.homescreenskilldata/page.txt') as f:
         file = f.read()
-        if (file == ""):
+        if (file == "\n"):
             oude = "homescreen"
             driver.get("file:///opt/mycroft/skills/X-man-homescreen-css/homescreen.html")
         if(file == "homescreen" and oude != "homescreen"):
