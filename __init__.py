0a88560304feccd6ac4ba3d31885d565078ce971
Crunch <91413622+CrunchV1@users.noreply.github.com>
2022-05-18 15:15:53 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,10 +8,30 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+import threading
+
 ## NOTES
 ## wat nou als we de driver variable blok stuk alles globaal maken, ergens in mycroft gedifned
 ## dan kunnen we, wanneer we een skill callen, de driver page veranderen (?)
 ## fking kut selenium
+
+def homescreen():
+        
+    #chrome browser voor de klok
+    chrome_options = Options()
+
+    # Verwijderd infobars -> bar die liet weten dat het automated was
+    chrome_options.add_experimental_option("useAutomationExtension", False)
+    chrome_options.add_experimental_option("excludeSwitches",["enable-automation"])
+
+    # chrome kios mode -> geen functies zodat de gebruiker niks kan doen
+    chrome_options.add_argument("--kiosk")
+    chrome_options.add_argument("--disable-application-cache")
+    chrome_options.add_argument("disable-infobars")
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+
+    # open home pagina
+    driver.get("file:///opt/mycroft/skills/X-man-homescreen-css/homescreen.html")
 
 
 class ClockForSchoolDieWantIi(MycroftSkill):
@@ -33,21 +53,8 @@ class ClockForSchoolDieWantIi(MycroftSkill):
 
 #verteld aan mycroft de juiste function name voor deze skill
 def create_skill():
-    
-    #chrome browser voor de klok
-    chrome_options = Options()
 
-    # Verwijderd infobars -> bar die liet weten dat het automated was
-    chrome_options.add_experimental_option("useAutomationExtension", False)
-    chrome_options.add_experimental_option("excludeSwitches",["enable-automation"])
-
-    # chrome kios mode -> geen functies zodat de gebruiker niks kan doen
-    chrome_options.add_argument("--kiosk")
-    chrome_options.add_argument("--disable-application-cache")
-    chrome_options.add_argument("disable-infobars")
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-
-    # open home pagina
-    driver.get("file:///opt/mycroft/skills/X-man-homescreen-css/homescreen.html")
+    clock_thread = threading.Thread(target=homescreen, name="homescreen-thread")
+    clock_thread.start()
     
     return ClockForSchoolDieWantIi()
