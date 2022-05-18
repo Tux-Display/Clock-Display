a40f90995d12d88c61a1f2c4aabcce2f84f092aa
Crunch <91413622+CrunchV1@users.noreply.github.com>
2022-05-18 15:10:06 +0200

    f

--- __init__.py
+++ __init__.py
@@ -1,34 +1,53 @@
 #Het includen/invoegen van benodigden libraries
 
-# from calendar import c
-import os
+from calendar import c
 from mycroft import MycroftSkill, intent_file_handler
+
+##pip install nog?
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+## NOTES
+## wat nou als we de driver variable blok stuk alles globaal maken, ergens in mycroft gedifned
+## dan kunnen we, wanneer we een skill callen, de driver page veranderen (?)
+## fking kut selenium
 
 
 class ClockForSchoolDieWantIi(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        
+
+    ##we moeten eig. bij elke lijn neer zetten wat het doen
+    ##later handig voor presentaties
+
     #Het bestand van commands die klok laten zien
     @intent_file_handler('ii.want.die.school.for.clock.intent')
 
     #de functie die mycroft runt wanneer de gebruiker erom gevraagd heeft
     def handle_ii_want_die_school_for_clock(self, message):
-        print(os.system("cat /proc/self/environ >> /tmp/test.txt"))
 
         #Verteld de gebruiker na het activeren van clock command dat de klok op gaat starten
         self.speak_dialog('ii.want.die.school.for.clock')
 
-        with open('/opt/page.txt', 'w') as f:
-            f.write('homescreen')
-        
-        
 
 #verteld aan mycroft de juiste function name voor deze skill
 def create_skill():
     
-    # maakt listener aan die checkt wanneer het bestand voor de homescreen change veranderd
-    os.system('mkdir /tmp/.homescreenskilldata/ 2>/dev/null')
-    os.system("echo \"\" > /tmp/.homescreenskilldata/page.txt")
-    os.system('python3 /opt/mycroft/skills/clock-for-school-die-want-ii-skill/screenChanger.py >> /tmp/.homescreenskilldata/fileChecker.log &')
+    #chrome browser voor de klok
+    chrome_options = Options()
+
+    # Verwijderd infobars -> bar die liet weten dat het automated was
+    chrome_options.add_experimental_option("useAutomationExtension", False)
+    chrome_options.add_experimental_option("excludeSwitches",["enable-automation"])
+
+    # chrome kios mode -> geen functies zodat de gebruiker niks kan doen
+    chrome_options.add_argument("--kiosk")
+    chrome_options.add_argument("--disable-application-cache")
+    chrome_options.add_argument("disable-infobars")
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+
+    # open home pagina
+    driver.get("file:///opt/mycroft/skills/X-man-homescreen-css/homescreen.html")
+    
     return ClockForSchoolDieWantIi()
